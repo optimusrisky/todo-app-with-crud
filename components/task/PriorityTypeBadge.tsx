03eb01169fb2695d58d1df93302013bffe80613c
Ryosuke Futsuki <crayon155@gmail.com>
2026-04-29 17:52:07 +0900

    add: タスク優先度用バッジコンポーネントを追加した #3

---
+++ components/task/PriorityTypeBadge.tsx
@@ -0,0 +1,35 @@
+import { Badge } from "@/components/ui/badge";
+import type { Task } from "@/types/taskTypes";
+
+interface Props {
+  priorityType: Task["priorityType"];
+  children: React.ReactNode;
+}
+
+/**
+ * タスクの優先度バッジ
+ * @param priorityType タスクの優先度
+ * @@param children
+ */
+export const PriorityTypeBadge = ({ priorityType, children }: Props) => {
+  const getBgColor = () => {
+    switch (priorityType) {
+      case "high":
+        return "bg-priority-high";
+      case "medium":
+        return "bg-priority-medium";
+      case "low":
+        return "bg-priority-low";
+      default:
+        break;
+    }
+  };
+
+  return (
+    <Badge
+      className={`${getBgColor()} h-fit py-1 px-2 rounded-sm text-white font-bold`}
+    >
+      {children}
+    </Badge>
+  );
+};
