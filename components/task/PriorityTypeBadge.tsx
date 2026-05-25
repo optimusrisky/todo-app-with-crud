508f12c8a5749cabcc94d5b512a7aa802943c65f
Ryosuke Futsuki <crayon155@gmail.com>
2026-05-25 23:01:35 +0900

    refactor: #16 ダッシュボード画面をリファクタした

--- components/task/PriorityTypeBadge.tsx
+++ components/task/PriorityTypeBadge.tsx
@@ -1,17 +1,18 @@
 import { Badge } from "@/components/ui/badge";
+import { PRIORITY_LABEL } from "@/consts/task";
 import type { Task } from "@/types/taskTypes";
 
+type PriorityType = NonNullable<Task["priorityType"]>;
+
 interface Props {
-  priorityType: Task["priorityType"];
-  children: React.ReactNode;
+  priorityType: PriorityType;
 }
 
 /**
  * タスクの優先度バッジ
  * @param priorityType タスクの優先度
- * @@param children
  */
-export const PriorityTypeBadge = ({ priorityType, children }: Props) => {
+export const PriorityTypeBadge = ({ priorityType }: Props) => {
   const getBgColor = () => {
     switch (priorityType) {
       case "high":
@@ -25,11 +26,13 @@ export const PriorityTypeBadge = ({ priorityType, children }: Props) => {
     }
   };
 
+  if (!priorityType) return null;
+
   return (
     <Badge
       className={`${getBgColor()} h-fit py-1 px-2 w-20 rounded-sm text-white font-bold`}
     >
-      優先度：{children}
+      優先度：{PRIORITY_LABEL[priorityType]}
     </Badge>
   );
 };
