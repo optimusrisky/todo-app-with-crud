547df18095145f4316ae9c27ea3f4799f783d197
Ryosuke Futsuki <crayon155@gmail.com>
2026-05-31 18:21:20 +0900

    change: タスク作成ボタンのUIを変更した #19
    タスク詳細画面に合わせて修正した

--- app/(auth)/dashboard/create/CreateTaskForm.tsx
+++ app/(auth)/dashboard/create/CreateTaskForm.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
 import { DatePickerButton } from "@/components/DatePickerButton";
 import { Button } from "@/components/ui/button";
-import { Field, FieldGroup } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import {
   NativeSelect,
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/native-select";
 import { Textarea } from "@/components/ui/textarea";
 import { Paths } from "@/consts/consts";
+import { PRIORITY_LABEL, STATUS_LABEL } from "@/consts/task";
 
 export const CreateTaskForm = () => {
   const [date, setDate] = useState<Date | undefined>();
@@ -31,40 +32,77 @@ export const CreateTaskForm = () => {
   };
 
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <div className="border rounded-lg p-4">
-        <FieldGroup>
-          <Field orientation="horizontal">
-            <div className="min-w-[150px]">タスク名</div>
+    <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col gap-4">
+      <div className="flex gap-4 w-full">
+        <div className="border rounded-lg p-4 flex flex-col gap-4 min-h-50 w-[70%] flex-1">
+          <div className="flex flex-col gap-2">
+            <div className="min-w-[150px] font-bold">タスク名</div>
             <Input id="name" placeholder="タスク名を入力してください" />
-          </Field>
-          <Field orientation="horizontal" className="items-baseline">
-            <div className="min-w-[150px]">説明</div>
-            <Textarea id="description" placeholder="説明を入力してください" />
-          </Field>
-          <Field orientation="horizontal">
-            <div className="min-w-[150px]">優先度</div>
-            <NativeSelect id="priorityType" className="w-75">
-              <NativeSelectOption value="">優先度を選択</NativeSelectOption>
-              <NativeSelectOption value="low">低</NativeSelectOption>
-              <NativeSelectOption value="medium">中</NativeSelectOption>
-              <NativeSelectOption value="high">高</NativeSelectOption>
-            </NativeSelect>
-          </Field>
-          <Field orientation="horizontal">
-            <div className="min-w-[150px]">期日</div>
-            <DatePickerButton
-              onSelect={handleSelectDate}
-              selectedDate={date}
-              className="w-75"
-            />
-          </Field>
-          <div className="flex justify-end">
-            <Button type="submit" className="w-50 py-2 h-fit">
-              作成
-            </Button>
           </div>
-        </FieldGroup>
+          <div className="flex flex-col gap-2 flex-1 min-h-0">
+            <div className="min-w-[150px] font-bold">説明</div>
+            <Textarea
+              id="description"
+              placeholder="説明を入力してください"
+              className="field-sizing-fixed flex-1 min-h-0 h-full resize-none"
+            />
+          </div>
+        </div>
+        <div className="border rounded-lg p-4 flex flex-col gap-2 w-[30%]">
+          <h3 className="text-md font-bold">タスク情報</h3>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center">
+              <div className="min-w-[120px]">ステータス</div>
+              <NativeSelect id="statusType" className="w-full">
+                <NativeSelectOption value="">未設定</NativeSelectOption>
+                {Object.entries(STATUS_LABEL).map(([value, label]) => (
+                  <NativeSelectOption key={value} value={value}>
+                    {label}
+                  </NativeSelectOption>
+                ))}
+              </NativeSelect>
+            </div>
+            <div className="flex items-center">
+              <div className="min-w-[120px]">優先度</div>
+              <NativeSelect id="priorityType" className="w-full">
+                <NativeSelectOption value="">未設定</NativeSelectOption>
+                {Object.entries(PRIORITY_LABEL).map(([value, label]) => (
+                  <NativeSelectOption key={value} value={value}>
+                    {label}
+                  </NativeSelectOption>
+                ))}
+              </NativeSelect>
+            </div>
+            <div className="flex items-center">
+              <div className="min-w-[120px]">作成者</div>
+              <Input
+                id="createdBy"
+                placeholder="作成者を入力してください"
+                className="w-full"
+              />
+            </div>
+            <div className="flex items-center">
+              <div className="min-w-[120px]">期日</div>
+              <DatePickerButton
+                onSelect={handleSelectDate}
+                selectedDate={date}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <Button
+          asChild
+          variant="outline"
+          type="button"
+          className="w-50 py-2 h-fit"
+        >
+          <Link href={Paths.DASH_BOARD}>一覧に戻る</Link>
+        </Button>
+        <Button type="submit" className="w-50 py-2 h-fit">
+          作成
+        </Button>
       </div>
     </form>
   );
