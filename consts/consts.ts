a4b0c1887e162767eecf174ddaa0ba77ce99aef6
Ryosuke Futsuki <crayon155@gmail.com>
2026-06-07 14:23:09 +0900

    add: Pathsにタスク編集画面のパスを追加した
    - タスク編集画面のパスを追加
    - 各パスにJSDocを追加

--- consts/consts.ts
+++ consts/consts.ts
@@ -1,6 +1,12 @@
 export const Paths = {
+  /** ログイン */
   LOGIN: "/",
+  /** ダッシュボード */
   DASH_BOARD: "/dashboard",
+  /** タスク作成 */
   TASK_CREATE: "/dashboard/create",
+  /** タスク詳細 */
   TASK_DETAIL: "/dashboard/:id",
+  /** タスク編集 */
+  TASK_EDIT: "/dashboard/:id/edit",
 };
